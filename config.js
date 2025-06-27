e50b1d799dd34baa8ac46b543dad67dd4eca03df
Su-yeonPark <aelen101@hanmail.net>
2025-06-27 16:36:21 +0900

    config_update

--- config.js
+++ config.js
@@ -3,18 +3,18 @@ export const KAKAOTALK_API_TOKEN = "JavaScript 키 입력";
 export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
 
-export const WEDDING_DATE = "2026년 01월 17일, 토요일 오전 12시 00분";
-export const WEDDING_LOCATION = "플레이스 플로라, 유리온실";
+export const WEDDING_DATE = "2026년 01월 17일, 토요일 오전 1시 00분";
+export const WEDDING_LOCATION = "플레이스 플로라, 1층 유리온실";
 
 export const GROOM_NAME = "하대훈";
-export const GROOM_ACCOUNT_NUMBER = "○○은행 ***-***-******";
-export const GROOM_FATHER_NAME = "○○○";
+export const GROOM_ACCOUNT_NUMBER = "신한은행 ***-***-******";
+export const GROOM_FATHER_NAME = "하다다";
 export const GROOM_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
-export const GROOM_MOTHER_NAME = "○○○";
+export const GROOM_MOTHER_NAME = "와이와이";
 export const GROOM_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
 
 export const BRIDE_NAME = "박수연";
-export const BRIDE_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const BRIDE_ACCOUNT_NUMBER = "국민은행 ***-***-******";
 export const BRIDE_FATHER_NAME = "박응진";
 export const BRIDE_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
 export const BRIDE_MOTHER_NAME = "하경희";
